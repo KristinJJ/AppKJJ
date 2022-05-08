11cd085bed4027d5094d5902e79e839c82a00f4e
KristinJJ <kristin.jue@gmail.com>
2022-05-07 20:22:48 -0700

    Added Traffic Cam List Activity

--- app/src/main/java/com/example/ad_340/MainActivity.kt
+++ app/src/main/java/com/example/ad_340/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 const val EXTRA_MESSAGE = "com.example.ad_340.MESSAGE"
 
@@ -15,6 +21,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivity", "Starting MainActivity");
+
+        val textView = findViewById<TextView>(R.id.textView2);
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://web6.seattle.gov/Travelers/api/Map/Data?zoomId=13&type=2"
+
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener<JSONObject> { response ->
+                // Display the first 500 characters of the response string.
+                val value = response.getJSONArray("Features")
+                textView.text = "Response is: ${value.length()}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
 
     }
 
@@ -37,10 +62,10 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    /** called when the user taps the Movie List button **/
+    /** called when the user taps the Traffic Cam List button **/
     fun viewTrafficCams(view: View) {
         val message = "Traffic Cam List"
-        val intent = Intent(this, MovieList::class.java).apply {
+        val intent = Intent(this, TrafficCamList::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
         }
         startActivity(intent)
